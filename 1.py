39c07fa2b35055a5a01c4f298d11565391f2d7ed
xukeguo <82247014+xukeguo@users.noreply.github.com>
2022-04-04 13:11:04 +0800

    Merge branch 'main' of https://github.com/xukeguo/-1

--- 1.py
+++ 1.py
@@ -10,10 +10,12 @@ print(9+6)
 555555
 print(9-3)
 #00
-b =open("/Users/xkg/Documents/test.txt", "a+")
-print("520", file=a)
+b=open("/Users/xkg/Documents/test.txt", "a+")
+print("ggg", file=b)
 b.close()
-open()
+b=open("/Users/xkg/Documents/test.txt", "a+")
+print("ggg", file=b)
+b.close()
 
 
 
