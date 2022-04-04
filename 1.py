91cd8c4fa2e8fa1005a8265af1164d1bf6887bae
xukeguo <82247014+xukeguo@users.noreply.github.com>
2022-04-04 13:43:53 +0800

    o

--- 1.py
+++ 1.py
@@ -12,12 +12,9 @@ print(9-3)
 #00
 b=open("/Users/xkg/Documents/test.txt", "a+")
 print("ggg", file=b)
-<<<<<<< HEAD
 b.close()
 b=open("/Users/xkg/Documents/test.txt", "a+")
 print("ggg", file=b)
-=======
->>>>>>> c1b836090c87009814ff5ece675daf8283422750
 b.close()
 
 
