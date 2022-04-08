c7dab7e9b47fc8fab2c4e20e168166792f2f26a7
xukeguo <108842875@qq.com>
2022-04-08 22:50:05 +0800

    Update money.py

--- money.py
+++ money.py
@@ -53,6 +53,7 @@ if score>=90 and score<=100:
     print('优秀')
 elif score>=80 and score<=89:
     print('良好')
+    print('良好')
 elif score>=70 and score<=79:
     print('中等')
 elif score>=60 and score<=69:
@@ -76,8 +77,56 @@ elif 0<=score>=59:
 else:
     print('输入错误')
     #     多分支if-else
+    #嵌套if-else
+#判断是否为会员
+#会员等级
+#普通会员
+#银牌会员
+#金牌会员
+#钻石会员
+#输入
+answer=input('是否为会员?：y/n')
+money=float(input('请输入金额：'))
+if answer=='y' or answer=='Y':
+    if 2000>=money>=1000:
+        print('普通会员') #普通会员
+    elif  3000>= money>=2000:
+        print('银牌会员')
+    elif 4000>=money>=3000:
+        print('金牌会员')
+    elif money>=4000:
+        print('钻石会员')
+    else:
+        print('输入错误')
+else:
+    print('输入错误')
+    #嵌套if-else
+#判断是否为会员
+answer=input('是否为会员?：y/n')
+money=float(input('请输入金额：'))
+if answer=='Y' or answer=='y':
+    if money>=5000:
+        print('钻石会员五折',money*0.5)          
+    elif money>=4000:
+        print('金牌会员六折',money*0.6)
+    elif money>=3000:
+        print('银牌会员七折',money*0.7)
+    elif money>=2000:
+        print('普通会员八折',money*0.8)
+    elif money>=1:
+        print('普通会员九折',money*0.9)
+    else:
+        print('普通会员',money)
+elif answer=='N' or answer=='n':
+    print('普通会员',money)
+else:
+    print('输入错误')
+    #黑白名单
+    name=input('请输入姓名：')
+    if name in ['张三','李四','王五']:#判断是否为黑名单
+        print('黑名单') #黑名单
+    else: #不是黑名单
+        print('白名单') #白名单
+
     
-
- 
-
-
+    
