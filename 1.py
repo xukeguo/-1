25c288825621c5a74bb5a2a785c9ef68b4cb6419
xukeguo <108842875@qq.com>
2022-04-09 12:52:26 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -36,10 +36,12 @@ i=range(1,10,2)
 for j in i:
     print(j)
 #while循环
-i=0
-while i<10:
-    print(i)
-    i+=1
+i=0 #初始化循环变量 
+num_a=0  #初始化工作变量
+while i<101: #循环条件
+  num_a+=i #工作变量的操作（表达式）
+  i+=1     #循环变量操作与定义（表达式），   循环变量的操作（表达式）与工作变量的操作（表达式）统称为循环体
+print(num_a)  #循环结束后执行
 #break
 i=0
 while i<10:
@@ -74,14 +76,14 @@ for i in range(1,10,2): #多重定义，变化赋值
 sum1=0
 for i in range(2,101,2):
  sum1+=i #sum=sum+i sum1实现了迭代累加（累积）
-print(sum1)
+print(sum1)  #2550
 #累加
 sum2=0
 for i in range(1,101,2):
  sum2+=i
-print(sum2)
+print(sum2)  #2500
 sum3=sum1+sum2
-print(sum3)
+print(sum3)  #5050
 
 
 
