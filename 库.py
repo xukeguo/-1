3081d890e263ac404a1aab81ac4d8b96615d2d26
xukeguo <108842875@qq.com>
2022-04-13 22:08:49 +0800

    Create 库.py

---
+++ 库.py
@@ -0,0 +1,6 @@
+list_mame=['Waiwu','Lisi','Wangwu','Zhaoliu','Zhangsan']
+list_age=[18,19,20,21,22]
+list_score=[90,80,70,60,50]
+for i in range(0,len(list_mame)):
+    print('姓名：',list_mame[i],'年龄：',list_age[i],'成绩：',list_score[i])
+# for i in range(len(list_mame)):
