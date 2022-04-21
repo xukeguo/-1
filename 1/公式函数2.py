617b8b3b87c68a1a22bc0c7b41cc98d5c35732ef
xukeguo <108842875@qq.com>
2022-04-21 22:18:11 +0800

    Update 公式函数2.py

--- 1/公式函数2.py
+++ 1/公式函数2.py
@@ -34,6 +34,7 @@ def main():
     turtle.circle(10)
     turtle.done()
 main()
+from cmath import cos
 import datetime#时间模块
 def main():#
     print(datetime.datetime.now())#当前时间
@@ -378,3 +379,30 @@ solv4('3x^2+60x+7=0')
 #shuffle(x)#将x的元素随机排序
 #choice(x)#从x中随机选取一个元素
 #sample(x,n)#从x中随机选取n个元素
+#已和任意数相乘的方式生成随机数  如：random.randrange(1,10,2)
+#random.randint(1,10)
+#已知不在同一直线上三点坐标，求圆心坐标
+class Point():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+ 
+ 
+def getCircle(p1, p2, p3):
+    x21 = p2.x - p1.x
+    y21 = p2.y - p1.y
+    x32 = p3.x - p2.x
+    y32 = p3.y - p2.y
+    # three colinear
+    if (x21 * y32 - x32 * y21 == 0):
+        return None
+    xy21 = p2.x * p2.x - p1.x * p1.x + p2.y * p2.y - p1.y * p1.y
+    xy32 = p3.x * p3.x - p2.x * p2.x + p3.y * p3.y - p2.y * p2.y
+    y0 = (x32 * xy21 - x21 * xy32) / (2 * (y21 * x32 - y32 * x21))
+    x0 = (xy21 - 2 * y0 * y21) / (2.0 * x21)
+    R = ((p1.x - x0) ** 2 + (p1.y - y0) ** 2) ** 0.5
+    return x0, y0, R
+ 
+ 
+p1, p2, p3 = Point(0, 0), Point(-4, 0), Point(0, -4)
+print(getCircle(p1, p2, p3))
